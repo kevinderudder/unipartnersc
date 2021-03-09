69f5fafdc4e1fd7103d4a7e20cc00cfe65ee3ef9
Kevin DeRudder <kevin_derudder@hotmail.com>
2021-03-09 13:04:48 +0100

    atm

---
+++ ATM/ATM.Logic/BankAccount.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM.Logic
+{
+    public class BankAccount
+    {
+        // velden of fields
+        private string accountNumber;
+        //private string firstName;
+        //private string lastName;
+        private double deposit;
+        private double savings;
+
+        // eigenschappen of properties
+        public string AccountNumber
+        {
+            get
+            {
+                return accountNumber;
+            }
+            set {
+                accountNumber = value;
+            }
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Deposit {
+            get {
+                return deposit;
+            }
+            private set {
+                if (value <= 0) { deposit = 0; }
+                else { deposit = value; }
+            }
+        }
+        public double Savings {
+            get {
+                return savings;
+            }
+            private set {
+                if (value <= 0) { savings = 0; }
+                else { savings = value; }
+            }
+        }
+
+        // constructor
+        public BankAccount(string accountNumber) {
+            this.AccountNumber = accountNumber;
+            this.Deposit = 0;
+            this.Savings = 0;
+        }
+
+        // methodes
+        public void AddToDeposit(double amount) {
+            //this.Deposit = this.Deposit + amount;
+            this.Deposit += amount;
+        }
+
+        public void AddToSavings(double amount) {
+            this.Savings += amount;
+        }
+
+        public void WithdrawFromDeposit(double amount) {
+            if (amount > this.Deposit)
+            {
+                //TODO: unsufficient funds
+            }
+            else {
+                this.Deposit -= amount;
+            }
+        }
+
+        public void WithdrawFromSavings(double amount) {
+            if (amount > this.Savings)
+            {
+                //TODO: unsufficient funds
+            }
+            else {
+                this.Savings -= amount;
+            }
+        }
+
+
+        // overrides
+        public override string ToString() {
+            return $"Uw saldo bedraagt: \n Zichtrekening: {this.Deposit} \n Spaarrekening: {this.Savings}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            BankAccount ba = obj as BankAccount;
+            if (ba == null)
+            {
+                return false;
+            }
+            else if (ba.AccountNumber == this.AccountNumber)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
---
+++ ATM/ATM.Ui/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using ATM.Logic;
+
+namespace ATM.Ui
+{
+    class Program
+    {
+
+        static BankAccount bankAccount;
+
+        static void Main(string[] args)
+        {
+            LoadBankAccount();
+            ShowMainMenu();
+        }
+
+        static void LoadBankAccount() {
+            bankAccount = new BankAccount("12345678");
+        }
+
+        static void ShowMainMenu() {
+
+            string input;
+
+            do {
+                Console.WriteLine("> 1. Haal geld van zichtrekening");
+                Console.WriteLine("> 2. Haal geld van spaarrekening");
+                Console.WriteLine("> 3. Stort geld op zichtrekening");
+                Console.WriteLine("> 4. Stort geld op spaarrekening");
+                Console.WriteLine("> 5. Saldoweergave");
+                Console.WriteLine("> Maak een keuze, druk X om te stoppen");
+
+                input = Console.ReadLine();
+
+                if (input.ToUpper() == "X")
+                {
+                    Console.WriteLine("Bedankt, prettige dag nog.");
+                }
+                else
+                {
+                    //int choice = Convert.ToInt32(input);
+                    int choice = 5;
+                    Int32.TryParse(input, out choice);
+
+                    switch (choice) {
+                        case 1: WithdrawFromDeposit();break;
+                        case 2: WithdrawFromSavings(); break;
+                        case 3: AddToDeposit();break;
+                        case 4: AddToSavings();break;
+                        case 5:
+                        default: Console.WriteLine(bankAccount);break;
+                    }
+
+                    Console.WriteLine($"bedankt voor u keuze: {choice}");
+                }
+            }
+            while (input.ToUpper() != "X");
+            
+
+        }
+
+        public static void AddToDeposit() {
+            double amount = ShowHowMuchMoneyLine(false);
+            bankAccount.AddToDeposit(amount);
+            Console.WriteLine($"uw zichtrekeningsaldo is {bankAccount.Deposit}€");
+        }
+
+        public static void AddToSavings()
+        {
+            double amount = ShowHowMuchMoneyLine(false);
+            bankAccount.AddToSavings(amount);
+            Console.WriteLine($"uw spaarrekeningsaldo is {bankAccount.Savings}€");
+        }
+
+        public static void WithdrawFromDeposit() {
+            double amount = ShowHowMuchMoneyLine(true);
+            bankAccount.WithdrawFromDeposit(amount);
+            Console.WriteLine($"uw zichtrekeningsaldo is {bankAccount.Deposit}€");
+        }
+
+        public static void WithdrawFromSavings()
+        {
+            double amount = ShowHowMuchMoneyLine(true);
+            bankAccount.WithdrawFromSavings(amount);
+            Console.WriteLine($"uw spaarrekeningsaldo is {bankAccount.Savings}€");
+        }
+
+        public static double ShowHowMuchMoneyLine(bool isWithdraw) {
+            if (isWithdraw)
+            {
+                Console.WriteLine("> Hoeveel geld had u willen opnemen");
+            }
+            else {
+                Console.WriteLine("> Hoeveel geld had u willen storten");
+            }
+            
+            string input = Console.ReadLine();
+            double amount = 0;
+            double.TryParse(input, out amount);
+
+            return amount;
+        }
+
+    }
+}
